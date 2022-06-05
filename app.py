d8dfe0fd8b1ae29912fd570a9f61aff914544c63
Chandra mohan <chandramohantn23@gmail.com>
2022-06-05 12:02:33 +0530

    Added github workflows

---
+++ app.py
@@ -0,0 +1,55 @@
+import os
+import yaml
+import joblib
+import numpy as np
+from flask import Flask, render_template, request, jsonify
+
+params_path = "params.yaml"
+webapp_path = "webapp"
+
+static_dir = os.path.join(webapp_path, "static")
+template_dir = os.path.join(webapp_path, "templates")
+
+app = Flask(__name__, static_folder=static_dir, template_folder=template_dir)
+
+def read_params(config_path):
+    with open(config_path, 'r') as yaml_file:
+        config = yaml.safe_load(yaml_file)
+    return config
+
+
+def predict(data):
+    config = read_params(params_path)
+    model_dir_path = config["webapp_model_dir"]
+    model = joblib.load(model_dir_path)
+    print(prediction[0])
+    return prediction[0]
+
+
+def api_response(request):
+    try:
+        data = np.array([list(request.json.values())])
+        response = predict(data)
+        response = {"response": response}
+    except Exception as e:
+        print(e)
+        error = {"error": "Something went wrong!! Try again!!"}
+
+
+@app.route("/", methods=["GET", "POST"])
+def index():
+    if request.method == "POST":
+        try:
+            if request.form:
+                data = dict(request.form).values()
+                data = [list(map(float, data))]
+                response = predict(data)
+                return render_template("index.html", response=response)
+            elif request.json:
+                response = api_response(request)
+                return jsonify(response)
+    else:
+        return render_template("index.html")
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000, debug=True)
